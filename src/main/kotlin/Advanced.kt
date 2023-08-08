74aae4b9d270612b44579a38df7374d4b6df9e5e
jungwon <mac20010@naver.com>
2023-08-08 14:24:19 +0900

    FEAT : lambda

---
+++ src/main/kotlin/Advanced.kt
@@ -0,0 +1,15 @@
+
+// 1. 람다
+
+//val square : (Int) -> (Int) = {number ->number*number}
+val square = {number :Int ->number*number}
+
+val nameAge = {name:String, age:Int ->
+    "my name is $name I'm $age"
+}
+
+fun main(){
+    println(square(12))
+    println(nameAge("joyce",99))
+}
+
