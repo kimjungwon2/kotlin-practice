16cec42c312218dca6002ab949d42ac8fce2152f
jungwon <mac2001035@gmail.com>
2024-03-27 20:08:42 +0900

    FEAT: 코틀린에는 null이 올 때, ? 붙여야.

---
+++ beginner/KotlinStart/src/main/kotlin/KotlinNull.kt
@@ -0,0 +1,29 @@
+import java.lang.IllegalArgumentException
+
+fun main(){
+
+}
+
+fun startsWithA1(str: String?): Boolean{
+    if (str == null) {
+        throw IllegalArgumentException("null이 들어왔습니다.")
+    }
+
+    return str.startsWith("A")
+}
+
+fun startsWithA2(str: String?): Boolean?{
+
+    if(str == null){
+        return null;
+    }
+
+    return str.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean{
+    if(str == null){
+        return false
+    }
+    return str.startsWith("A")
+}
