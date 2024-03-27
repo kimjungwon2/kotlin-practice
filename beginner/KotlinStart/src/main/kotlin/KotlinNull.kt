c5f39662973671d1ca40dac9184882a54a6d6f78
jungwon <mac2001035@gmail.com>
2024-03-28 01:12:16 +0900

    FEAT: Safe Call과 Elvis 연산자로 개선.

--- beginner/KotlinStart/src/main/kotlin/KotlinNull.kt
+++ beginner/KotlinStart/src/main/kotlin/KotlinNull.kt
@@ -16,6 +16,12 @@ fun startsWithA1(str: String?): Boolean{
     return str.startsWith("A")
 }
 
+fun improveStartsWithA1(str: String?): Boolean{
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException("null이 들어왔습니다.")
+}
+
+
 fun startsWithA2(str: String?): Boolean?{
 
     if(str == null){
@@ -25,9 +31,17 @@ fun startsWithA2(str: String?): Boolean?{
     return str.startsWith("A")
 }
 
+fun improveStartsWithA2(str: String?): Boolean?{
+    return str?.startsWith("A")
+}
+
 fun startsWithA3(str: String?): Boolean{
     if(str == null){
         return false
     }
     return str.startsWith("A")
+}
+
+fun improveStartsWithA3(str: String?): Boolean{
+    return str?.startsWith("A") ?: false
 }
