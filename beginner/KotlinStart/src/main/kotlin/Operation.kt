21510a0256ca4bbe75757baec863e04e2549507a
jungwon <mac2001035@gmail.com>
2024-03-31 00:49:36 +0900

    FEAT: 동일성, 동등성 비교.

--- beginner/KotlinStart/src/main/kotlin/Operation.kt
+++ beginner/KotlinStart/src/main/kotlin/Operation.kt
@@ -6,10 +6,24 @@ fun main(){
     if (money1 > money2){
         println("Money1이 Money2보다 금액이 큽니다.")
     }
+
+    val mon1 = JavaMoney(1_000L)
+    val mon2 = mon1
+    val mon3 = JavaMoney(1_000L)
+
+    println(mon1 === mon2)
+    println(mon1 === mon3)
+    println(mon1 == mon3)
 }
 
 class JavaMoney(val amount: Long) : Comparable<JavaMoney> {
     override fun compareTo(other: JavaMoney): Int {
         return this.amount.compareTo(other.amount)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is JavaMoney) return false
+        return this.amount == other.amount
     }
 }
