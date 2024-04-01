200592ae7204ebded8e4c2a6e3e0fa4cffef30f3
jungwon <mac2001035@gmail.com>
2024-04-02 00:14:49 +0900

    FEAT: 코틀린 IF return 문.

--- beginner/KotlinStart/src/main/kotlin/Conditional.kt
+++ beginner/KotlinStart/src/main/kotlin/Conditional.kt
@@ -4,3 +4,22 @@ fun validateScoreIsNotNegative(score: Int){
     }
 }
 
+fun getPassOrFail(score: Int): String{
+    return if(score>=50){
+        return "P"
+    } else{
+        return "F"
+    }
+}
+
+fun getGrade(score: Int): String{
+    return if(score>=90){
+        "A"
+    }else if(score>=80){
+        "B"
+    }else if(score>=70){
+        "C"
+    }else{
+        "D"
+    }
+}
