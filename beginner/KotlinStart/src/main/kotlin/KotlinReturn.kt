c024108394db893c1e94bc24bb7559aff86308c9
jungwon <mac2001035@gmail.com>
2024-04-01 18:45:05 +0900

    FEAT: 논리 연산자

---
+++ beginner/KotlinStart/src/main/kotlin/KotlinReturn.kt
@@ -0,0 +1,16 @@
+fun main(){
+    if (fun1() || fun2()){
+        println("본문")
+    }
+
+}
+
+fun fun1(): Boolean{
+    println("fun 1")
+    return true
+}
+
+fun fun2(): Boolean{
+    println("fun 2")
+    return false
+}
