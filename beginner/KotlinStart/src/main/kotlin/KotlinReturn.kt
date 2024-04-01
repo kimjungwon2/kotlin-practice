de05de1e845aae78dbbae0e92ce698dc198fa50d
jungwon <mac2001035@gmail.com>
2024-04-01 18:50:31 +0900

    FEAT: 객체 연산자.

--- beginner/KotlinStart/src/main/kotlin/KotlinReturn.kt
+++ beginner/KotlinStart/src/main/kotlin/KotlinReturn.kt
@@ -3,6 +3,9 @@ fun main(){
         println("본문")
     }
 
+    val money1 = Money(1_000L)
+    val money2 = Money(2_000L)
+    println(money1 + money2)
 }
 
 fun fun1(): Boolean{
@@ -13,4 +16,20 @@ fun fun1(): Boolean{
 fun fun2(): Boolean{
     println("fun 2")
     return false
+}
+
+class Money(val amount: Long) : Comparable<JavaMoney> {
+    override fun compareTo(other: JavaMoney): Int {
+        return this.amount.compareTo(other.amount)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is JavaMoney) return false
+        return this.amount == other.amount
+    }
+
+    operator fun plus(money2: Money): Any {
+        return this.amount + money2.amount
+    }
 }
