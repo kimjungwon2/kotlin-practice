8a384286d58a04923082744887ac47b3e70bd5b8
jungwon <mac2001035@gmail.com>
2024-04-04 00:34:44 +0900

    FEAT: 전통적인 for문.

---
+++ beginner/KotlinStart/src/main/kotlin/lecture06/ForEachKotlin.kt
@@ -0,0 +1,19 @@
+package lecture06
+
+fun main(){
+    val numbers = listOf(1L,2L,3L)
+
+    for(number in numbers){
+        println(number)
+    }
+
+    for (i in 1..3){
+        println(i)
+    }
+
+    for (i in 3 downTo 1){
+        println(i)
+    }
+
+
+}
