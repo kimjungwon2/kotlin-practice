8bc53d3c7511298ddceedc530f29e9458eca7618
jungwon <mac2001035@gmail.com>
2024-04-05 00:49:44 +0900

    FEAT: 코틀린 예외 처리.

---
+++ beginner/KotlinStart/src/main/kotlin/lecture07/parseIntOrThrow.kt
@@ -0,0 +1,12 @@
+package lecture07
+
+import java.lang.NumberFormatException
+
+fun parseIntOrThrow(str: String): Int{
+    try{
+        return str.toInt();
+    }catch(e: NumberFormatException){
+        throw IllegalArgumentException("주어진 ${str}는 숫자가 아닙니다")
+    }
+}
+
