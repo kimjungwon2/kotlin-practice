0d03680610443e3c61fdcdefaa239a7c4094b79f
jungwon <mac2001035@gmail.com>
2024-04-07 21:28:33 +0900

    FEAT: Default 파라미터.

---
+++ beginner/KotlinStart/src/main/kotlin/lecture08/OverLoading.kt
@@ -0,0 +1,19 @@
+package lecture08
+
+fun main(){
+    repeat("Hello World")
+}
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+){
+    for (i in 1..num){
+        if(useNewLine){
+            println(str)
+        } else{
+            print(str)
+        }
+    }
+}
