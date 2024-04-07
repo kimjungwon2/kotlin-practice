2d86988f86908bbe229c4933d6bffe1aa0739766
jungwon <mac2001035@gmail.com>
2024-04-08 00:48:12 +0900

    FEAT: Named Argument (2)

---
+++ beginner/KotlinStart/src/main/kotlin/lecture08/ArgumentFunction.kt
@@ -0,0 +1,11 @@
+package lecture08
+
+fun main(){
+    printNameAndGender(gender="남성",name="김정원")
+}
+
+fun printNameAndGender(name: String, gender: String){
+    println(name)
+    println(gender)
+
+}
