210af06072669a5bb0d2d71156636a83a89337e7
jungwon <mac2001035@gmail.com>
2024-04-09 00:41:55 +0900

    FEAT: getter,setter.

--- beginner/KotlinStart/src/main/kotlin/lecture09/Person.kt
+++ beginner/KotlinStart/src/main/kotlin/lecture09/Person.kt
@@ -1,5 +1,12 @@
 package lecture09
 
+fun main(){
+    val person = Person("최태현",100)
+    println(person.name)
+    person.age = 10
+    println(person.age)
+}
+
 class Person(
     val name: String,
     var age:Int
