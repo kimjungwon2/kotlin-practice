87a36edd7f4fa8ce49c614b7381115342880a95c
jungwon <mac2001035@gmail.com>
2024-04-11 02:34:04 +0900

    FEAT: 추상 클래스

---
+++ beginner/KotlinStart/src/main/kotlin/lecture10/Penguin.kt
@@ -0,0 +1,16 @@
+package lecture10
+
+class Penguin(
+    species: String
+): Animal(species,2) {
+
+    private val wingCount: Int = 2
+
+    override fun move(){
+        println("펭귄이 움직인다~ 꿱꿱")
+    }
+
+    override val legCount: Int
+        get() = super.legCount + this.wingCount
+
+}
