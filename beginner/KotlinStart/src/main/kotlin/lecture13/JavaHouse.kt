4b5d2cd9a5c553213746b126c2eda93ae1dd1929
jungwon <mac2001035@gmail.com>
2024-04-15 20:03:54 +0900

    FEAT: 코틀린의 inner.

--- beginner/KotlinStart/src/main/kotlin/lecture13/JavaHouse.kt
+++ beginner/KotlinStart/src/main/kotlin/lecture13/JavaHouse.kt
@@ -6,7 +6,10 @@ class JavaHouse(
     private val address: String,
     private val livingRoom: LivingRoom
 ){
-    class LivingRoom(
+    inner class LivingRoom(
         private val area: Double
-    )
+    ){
+        val address: String
+            get() = this@JavaHouse.address
+    }
 }
