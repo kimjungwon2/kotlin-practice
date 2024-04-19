60c00494fab6455b0fbc1557c27a58f88248aa97
jungwon <mac2001035@gmail.com>
2024-04-19 19:36:41 +0900

    FEAT: Kotlin의 Map.

---
+++ beginner/KotlinStart/src/main/kotlin/lecture15/KotlinMap.kt
@@ -0,0 +1,13 @@
+package lecture15
+
+fun main(){
+
+    val oldMap = mutableMapOf<Int, String>()
+    oldMap.put(1,"Monday")
+    oldMap[2] = "Tuesday"
+
+    mapOf(1 to "MONDAY", 2 to "TUESDAY")
+
+
+
+}
