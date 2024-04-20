5c127946f8d2813bca6b0f3a353c4a51ab071ce0
jungwon <mac2001035@gmail.com>
2024-04-21 00:43:47 +0900

    FEAT: inline 함수.

--- beginner/KotlinStart/src/main/kotlin/lecture16/NextYear.kt
+++ beginner/KotlinStart/src/main/kotlin/lecture16/NextYear.kt
@@ -5,10 +5,11 @@ fun main(){
     3 add2 4
 }
 
-fun Int.add(other:Int): Int{
+inline fun Int.add(other:Int): Int{
     return this + other
 }
 
 infix fun Int.add2(other:Int):Int{
     return this + other
 }
+
