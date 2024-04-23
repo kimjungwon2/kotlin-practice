83378b0e11d297f49c90ea83e8a32c09943e02ce
jungwon <mac2001035@gmail.com>
2024-04-23 21:53:19 +0900

    FEAT: 코틀린에서의 람다.

--- beginner/KotlinStart/src/main/kotlin/lecture17/lambda.kt
+++ beginner/KotlinStart/src/main/kotlin/lecture17/lambda.kt
@@ -12,15 +12,31 @@ fun main(){
         Fruit("수박",10_000),
     )
 
+
+
     val isApple = fun(fruit: Fruit): Boolean{
         return fruit.name == "사과"
     }
 
-    val isApple2 = {fruit: Fruit -> fruit.name == "사과"}
+    val isApple2: (Fruit) -> Boolean = {it.name == "사과"}
 
-    isApple(fruits[0])
-    isApple.invoke(fruits[0])
+    filterFruits(fruits, isApple2)
 
+
+}
+
+private fun filterFruits(
+    fruits: List<Fruit>, filter: (Fruit)-> Boolean
+): List<Fruit>{
+    val results = mutableListOf<Fruit>()
+
+    for (fruit in fruits){
+        if(filter(fruit)){
+            results.add(fruit)
+        }
+    }
+
+    return results
 }
 
 class Fruit(val name:String, val price:Int)
