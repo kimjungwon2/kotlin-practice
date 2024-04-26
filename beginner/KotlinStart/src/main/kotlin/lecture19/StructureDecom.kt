04bc918c965cf6b6ad56432e669cfd3429a965be
jungwon <mac2001035@gmail.com>
2024-04-27 01:45:57 +0900

    FEAT: 구조 번호.

--- beginner/KotlinStart/src/main/kotlin/lecture19/StructureDecom.kt
+++ beginner/KotlinStart/src/main/kotlin/lecture19/StructureDecom.kt
@@ -7,7 +7,10 @@ data class Person(
 
 fun main(){
     val person = Person("김정원",100)
-    val (name, age) = person
+//    val (name, age) = person
+    val name = person.component1()
+    val age = person.component2()
+
     println("이름: ${name}, 나이: ${age}")
 
 }
