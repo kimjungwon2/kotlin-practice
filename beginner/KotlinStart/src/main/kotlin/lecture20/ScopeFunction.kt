a305c8ea00379bcd65344ba6672f29e2c1038f28
jungwon <mac2001035@gmail.com>
2024-04-30 01:50:36 +0900

    FEAT: scope function의 분류

--- beginner/KotlinStart/src/main/kotlin/lecture20/ScopeFunction.kt
+++ beginner/KotlinStart/src/main/kotlin/lecture20/ScopeFunction.kt
@@ -4,6 +4,24 @@ import lecture19.Person
 
 fun main(){
 
+    val person = Person("김순철",30)
+
+    val value1 = person.let{
+        it.age
+    }
+
+    val value2 = person.run{
+        this.age
+    }
+
+    val value3 = person.also{
+        it.age
+    }
+
+    val value4 = person.apply{
+        this.age
+    }
+
 }
 
 fun printPerson(person: Person?){
@@ -16,4 +34,8 @@ fun printPerson(person: Person?){
         println(person.name)
         println(person.age)
     }
+
+
+
 }
+
